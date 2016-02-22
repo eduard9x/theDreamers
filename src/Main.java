8e20b5936fb233bda47202e3dc9faf65f990278f
w1497109@my.westminster.ac.uk <w1497109@my.westminster.ac.uk>
2016-02-22 19:57:00 +0000

    Data base class and library. You might not be able to access data base. You need to know how via local host

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String[] args) {
         //javax.swing.SwingUtilities.invokeLater(new Runnable(){
         //public void run(){
+        // data base class
+        DB_Connect connect = new DB_Connect();
+        connect.getData();
+
         GUI gui = new GUI();
 
     }
