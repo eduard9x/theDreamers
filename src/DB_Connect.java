fda281c482db0ab7f7db78d52560864119ad5aea
eduard9x <ghinea.eduard@yahoo.com>
2016-02-29 17:05:17 +0000

    merge

--- src/DB_Connect.java
+++ src/DB_Connect.java
@@ -1,4 +1,5 @@
 import java.sql.*; // imports all the sql classes
+import java.util.Scanner;
 
 /**
  * Created by Harvey on 22/02/2016.
@@ -16,6 +17,9 @@ public class DB_Connect {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/theDreamers", "root", "");
+            //connection to my personal database
+            //con = DriverManager.getConnection("jdbc:mysql://hareshvekriyacom.ipagemysql.com/the_dreamers", "hareshvekriyacom", "thedreamers");
+
             st = con.createStatement();
 
         } catch (Exception e) {
@@ -25,11 +29,13 @@ public class DB_Connect {
 
     public void getData() {
         try {
-            double questionNum = Math.random() * 4 + 1;
+            Scanner input = new Scanner( System.in );
+            double questionNum = Math.random() * 2 + 1;
             int random = (int) questionNum;
-            int squareNum = random; // its random printing from 1-4, input your number here if you want specific row out of 4
+            System.out.println("Please enter the question number out 1-2");
+            int squareNum = input.nextInt(); // its random printing from 1-4, input your number here if you want specific row out of 4
 //            String query = "SELECT * FROM test";
-            String query = "SELECT * FROM test WHERE QNumber=" + squareNum;
+            String query = "SELECT * FROM theDreamers WHERE id=" + squareNum;
             rs = st.executeQuery(query);
             System.out.println("Records from the database");
 
@@ -37,7 +43,8 @@ public class DB_Connect {
             while (rs.next()) {
                 String question = rs.getString("Questions");
                 String answers = rs.getString("Answers");
-                System.out.println(question + " " + answers);
+                System.out.println("Question: "+question);
+                System.out.println("Answer: "+answers);
             }
 
         } catch (Exception e) {
